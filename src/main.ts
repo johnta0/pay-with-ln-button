4fa4f2a6adde4b76296ff6e209fa681f0dac8a1a
johnta0 <22534726+johnta0@users.noreply.github.com>
2018-10-02 17:04:16 +0900

    Simplify .gitignore

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port: number = 3000;
+  await app.listen(port);
+  logger.log(`Hosting on localhost:${port}`);
 }
 bootstrap();
