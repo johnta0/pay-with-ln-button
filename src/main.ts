88bfabb6f59d2cd00e7b0f4f0ac49496e38c4d5c
johnta0 <22534726+johnta0@users.noreply.github.com>
2018-10-05 14:36:02 +0900

    Add view

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { join } from 'path';
 
 const logger = new Logger();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port: number = 3000;
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
   await app.listen(port);
   logger.log(`Hosting on localhost:${port}`);
 }
